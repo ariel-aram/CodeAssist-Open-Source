557c474616279d7f1265e576a361e9303f383b14
nyxcoding <mmarleyapple@icloud.com>
2026-07-22 07:13:48 +0100

    Create getGlobals.js

---
+++ src/events/getGlobals.js
@@ -0,0 +1,21 @@
+events = {
+    name: "getGlobals",
+    description: "Gets global variables.",
+    params: [
+        {
+            name: "mainVar",
+            description: "Sets the main variable",
+            type: "String",
+            required: true,
+            rest: false
+        }
+        
+    ],
+    brackets: true,
+    code: `
+    $setGlobalVar[commandsUsed;$math[$getGlobalVar[commandsUsed]+1]]
+    $setGlobalVar[$env[mainVar];$math[$getGlobalVar[$env[mainVar]]+1]]
+    $setUserVar[userCommands;$math[$getUserVar[userCommands]+1]]
+    `
+}
+module.exports = events
